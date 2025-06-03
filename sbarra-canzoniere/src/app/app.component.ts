8d61979ee1c9477dc6455c5528b62732293f9d1b
MatteoSB11 jgjg <matteosbarra07@gmail.com>
2025-06-03 10:41:41 +0000

    Punto 4

--- sbarra-canzoniere/src/app/app.component.ts
+++ sbarra-canzoniere/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Canzoni } from './models/song.model';
+import { SongListComponent } from './song-list/song-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SongListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,23 +16,25 @@ export class AppComponent implements OnInit{
   title = 'sbarra-canzoniere';
   data! : Object;
    loading!: boolean;
-   obs! :Observable<Object>;
+   obs! :Observable<Canzoni[]>;
    http : HttpClient
+   vettCanzoni : Canzoni[] = []
 
    constructor(http : HttpClient){this.http = http}
 
    makeChiamata(){
     this.loading = true
-    this.obs = this.http.get('https://my-json-server.typicode.com/malizia-g/hotel/short-songlist')
+    this.obs = this.http.get<Canzoni[]>('https://my-json-server.typicode.com/malizia-g/hotel/short-songlist')
     this.obs.subscribe(this.getData)
    }
 
-   getData = (d : Object) => {
-    this.data = d
+   getData = (d : Canzoni[]) => {
+    this.vettCanzoni = d
     this.loading = false
-    console.log(this.data)
+    console.log(this.vettCanzoni)
    }
   ngOnInit(): void {
   this.makeChiamata()
-}
+ }
+
 }
---
+++ sbarra-canzoniere/src/app/song-list/song-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Canzoni } from '../models/song.model';
+
+@Component({
+  selector: 'app-song-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './song-list.component.html',
+  styleUrl: './song-list.component.css'
+})
+export class SongListComponent {
+  @Input() canz! : Canzoni
+
+}
