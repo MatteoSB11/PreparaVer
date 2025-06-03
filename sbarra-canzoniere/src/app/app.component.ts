48b26e65c7036bf8dc2b91b9e0b1df63b86ebc09
MatteoSB11 jgjg <matteosbarra07@gmail.com>
2025-06-03 10:30:24 +0000

    Punto 3

--- sbarra-canzoniere/src/app/app.component.ts
+++ sbarra-canzoniere/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,27 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'sbarra-canzoniere';
+  data! : Object;
+   loading!: boolean;
+   obs! :Observable<Object>;
+   http : HttpClient
+
+   constructor(http : HttpClient){this.http = http}
+
+   makeChiamata(){
+    this.loading = true
+    this.obs = this.http.get('https://my-json-server.typicode.com/malizia-g/hotel/short-songlist')
+    this.obs.subscribe(this.getData)
+   }
+
+   getData = (d : Object) => {
+    this.data = d
+    this.loading = false
+    console.log(this.data)
+   }
+  ngOnInit(): void {
+  this.makeChiamata()
+}
 }
